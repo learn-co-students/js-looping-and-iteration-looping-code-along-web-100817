301c123a985240653a5edefb941bc50c92697273
github username <github email address>
2017-11-20 16:04:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Code your solutions in this file
+
+function printBadges(employeeNames) {
+  for (let i = 0; i < employeeNames.length; i++) {
+    console.log(`Welcome ${employeeNames[i]}! You are employee #${i + 1}.`);
+  }
+  return employeeNames;
+}
+
+function tailsNeverFails() {
+  let i = 0;
+  while (Math.random() >= 0.5) {
+    i++;
+  }
+  return `You got ${i} tails in a row!`;
+}
