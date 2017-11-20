4a54c750cd36598316d17c113e6bc1463172e91b
Liam Killion <liam.killion@gmail.com>
2017-11-20 17:45:32 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+
+
+function printBadges(array){
+  for (var i = 0; i < array.length; i++) {
+    console.log(array[i])
+  }
+  return array
+}
+
+function tailsNeverFails(){
+  function coinflip () {
+    return Math.random() >= 0.5;
+  }
+  counter = 0
+  while (coinflip()){
+    counter++
+  }
+  return 'You got '+counter+' tails in a row!'
+}
